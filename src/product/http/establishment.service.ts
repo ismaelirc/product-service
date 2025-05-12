545aec1c33e6cfc2d14be8d2d99244c954b99033
Ismael Costa <ismaelirc@gmail.com>
2025-05-12 10:57:12 -0300

    seed and envs

--- src/product/http/establishment.service.ts
+++ src/product/http/establishment.service.ts
@@ -1,16 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EstablishmentClient {
-  constructor(private readonly http: HttpService) {}
+  constructor(
+    private readonly http: HttpService,
+    private config: ConfigService,
+  ) {}
 
   async checkIfExists(establishmentId: string): Promise<void> {
     try {
-      await firstValueFrom(
-        this.http.get(`http://establishmentservice:3000/v1/establishment/${establishmentId}`)
-      );
+      const url = this.config.get<string>('ESTABLISHMENT_SERVICE_URL');
+      if (!url) {
+        throw new Error('ESTABLISHMENT_SERVICE_URL is not defined');
+      }
+
+      await firstValueFrom(this.http.get(`${url}/${establishmentId}`));
     } catch (error) {
       throw new NotFoundException('Establishment not found');
     }
